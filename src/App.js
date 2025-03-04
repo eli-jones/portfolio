476406e995cbd379c1edcc907d937c14b7a59398
Elizabeth <ejones15@msn.com>
2025-03-04 12:32:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+
+import whiteStar from './assets/images/white-star.png'
+import goldStar from './assets/images/gold-star.png'
+import purpleStar from './assets/images/purple-star.png'
+
+import { Projects } from './components/projects'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <body>
+  <header>
+      <h1>Elizabeth Jones</h1>
+  </header>
+
+  <section className="home">
+      <img src={whiteStar} className="star white one" alt=''/>
+      <img src={whiteStar} className="star white two" alt=''/>
+      <img src={whiteStar} className="star white three" alt=''/>
+      <img src={purpleStar} className="star purple" alt=''/>
+      <img src={goldStar} className="star gold" alt=''/>
+      <h2>Welcome</h2>
+      <p>My name is Elizabeth. I'm a front end web developer based in Colorado. I love solving problems and making your dreams real.</p>
+  </section>
+
+  <section className="ph">
+      <p>Coming Soon.</p>
+  </section>
+    
+  <section className="work">
+      <Projects />
+  </section>
+    
+  <section className="contact">
+      <h2>Contact</h2>
+      <form method="post" action="https://forms.un-static.com/forms/0bb0aaa270a6ad697c25925285cf46e08584b008">
+          <label name="name">Name</label>
+          <input type="text" name="name" className="name" />
+          <label name="email">Email</label>
+          <input type="email" name="email" className="email" />
+          <label name="message">Your Message</label>
+          <textarea name="message" className="message"></textarea>
+          <input type="submit" name="submit" className="submit" value="Send" />
+          <p className="small"><small>(Powered by <a rel="nofollow" href="Un-static Forms">Un-static Forms</a>)</small></p>
+      </form>
+  </section>
+    
+</body>
   );
 }
 
---
+++ src/components/projects.js
@@ -0,0 +1,47 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './projects.css'
+
+import jamming from '../assets/images/jamming-app.PNG';
+import bhr from '../assets/images/bhr-home.PNG';
+import weather from '../assets/images/weather-app.PNG';
+
+function Projects() {
+    
+    const swiper = new Swiper('.swiper', {
+        modules: [Pagination],
+        grabCursor: true,
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            650: {
+                slidesPerView: 3,
+                spaceBetween: 60,
+            }
+        }
+    })
+    
+    return (
+        <div className='swiper'>
+            <div className='swiper-wrapper'>
+                <div className='swiper-slide'><a href='https://eli-jones-jammming.netlify.app' target='blank'><img src={jamming} alt='Jamming app'/><p>Jamming App ~ Spotify API</p></a></div>
+                <div className='swiper-slide'><a href='https://eli-jones-bhr.netlify.app/' target='blank'><img src={bhr} alt='Pet Rescue Site'/><p>Pet Rescue Site ~ React</p></a></div>
+                <div className='swiper-slide'><a href='https://eli-jones-weather.netlify.app/' target='blank'><img src={weather} alt='Weather App'/><p>Weather App ~ Open Weather API</p></a></div>
+            </div>
+            <div className='swiper-pagination'></div>
+        </div>
+    )
+    
+}
+
+export { Projects };
