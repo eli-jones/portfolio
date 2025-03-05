1472052ea2daa0b8963c63246e5c0286c05328d7
eli-jones <140195816+eli-jones@users.noreply.github.com>
2025-03-05 14:48:34 -0700

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
