3f1ce303ac4ebd5893cca7ab909db64acfd1cd81
eli-jones <140195816+eli-jones@users.noreply.github.com>
2025-03-05 14:33:43 -0700

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
-import Swiper from 'swiper';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -10,16 +11,15 @@ import weather from '../assets/images/weather-app.PNG';
 
 function Projects() {
     
-    const swiper = new Swiper('.swiper', {
-        modules: [Pagination],
-        grabCursor: true,
-        direction: 'horizontal',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        breakpoints: {
+    return (
+        <Swiper
+            cssMode={true}
+            pagination={true}
+            grabCursor={true}
+            direction={horizontal}
+            loop={true}
+            modules={Pagination}
+            breakpoints={{
             500: {
                 slidesPerView: 1,
                 spaceBetween: 10,
@@ -27,21 +27,13 @@ function Projects() {
             650: {
                 slidesPerView: 3,
                 spaceBetween: 60,
-            }
-        }
-    })
-    
-    swiper;
-    
-    return (
-        <div className='swiper'>
-            <div className='swiper-wrapper'>
-                <div className='swiper-slide'><a href='https://eli-jones-jammming.netlify.app' target='blank'><img src={jamming} alt='Jamming app'/><p>Jamming App ~ Spotify API</p></a></div>
-                <div className='swiper-slide'><a href='https://eli-jones-bhr.netlify.app/' target='blank'><img src={bhr} alt='Pet Rescue Site'/><p>Pet Rescue Site ~ React</p></a></div>
-                <div className='swiper-slide'><a href='https://eli-jones-weather.netlify.app/' target='blank'><img src={weather} alt='Weather App'/><p>Weather App ~ Open Weather API</p></a></div>
-            </div>
-            <div className='swiper-pagination'></div>
-        </div>
+            }}
+            className='swiper'
+        >
+            <SwiperSlide className='swiper-slide'><a href='https://eli-jones-jammming.netlify.app' target='blank'><img src={jamming} alt='Jamming app'/><p>Jamming App ~ Spotify API</p></a></SwiperSlide>
+            <SwiperSlide className='swiper-slide'><a href='https://eli-jones-bhr.netlify.app/' target='blank'><img src={bhr} alt='Pet Rescue Site'/><p>Pet Rescue Site ~ React</p></a></SwiperSlide>
+            <SwiperSlide className='swiper-slide'><a href='https://eli-jones-weather.netlify.app/' target='blank'><img src={weather} alt='Weather App'/><p>Weather App ~ Open Weather API</p></a></SwiperSlide>
+        </Swiper>
     )
     
 }
